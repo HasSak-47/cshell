8c8a15c0cef6098f9fd902705bb0b4e99d7afa8a
lilith machine <lilith.alanis.255@gmail.com>
2023-05-02 19:59:03 -0600

    finished basic formatter

---
+++ include/formatter.h
@@ -0,0 +1,10 @@
+#ifndef __FORMATER_H__
+#define __FORMATER_H__
+
+#include "object.h"
+
+DECLARE_FUNC_ID(format)
+char* format(char* fmt, void* ptr);
+void __format_setup();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,63 +1,38 @@
-#define _GNU_SOURCE
+#include "formatter.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <object.h>
+#include <stdarg.h>
+#include <generic_objects.h>
 
 #include <stdarg.h>
 
-#include <generic_objects.h>
+void print(const char* fmt, ...){
+	va_list v;
+	va_start(v, fmt);
+	size_t len = strlen(fmt);
+	for(size_t i = 0; i < len; ++i){
+		if(fmt[i] == '%'){
+			void* p = va_arg(v, void*);
+			char* c = format("", p);
+			printf("%s", c);
+			free(c);
+		}
+	}
 
-DEFINE_FUNC_ID(fmt)
-MKGOF(fmt)
-
-DEFINE_FUNC_ID(print)
-MKGOF(print)
-
-void* MFNT(fmt, i32)(void* ptr){
-	printf("int fucker!\n");
-	return NULL;
-}
-
-void* MFNT(fmt, str)(void* ptr){
-	printf("str fucker!\n");
-	return NULL;
-}
-
-void* MFNT(print, i32)(void* ptr){
-	printf("int shitter!\n");
-	return NULL;
-}
-
-void* MFNT(print, str)(void* ptr){
-	printf("str shitter!\n");
-	return NULL;
+	va_end(v);
 }
 
 int main(){
 	atexit(delete_tables);
-	// ---   setup   --- //
-	INIT_FUNC_ID(fmt);
-	INIT_FUNC_ID(print);
-
 	__init_generic_objects();
+	__format_setup();
 
+	INIT(i32, a, 10)
+	INIT(f32, b, 10)
+	print("% %", &a, &b);
 
-	OVERLOAD(fmt, i32)
-	OVERLOAD(fmt, str)
-	OVERLOAD(print, i32)
-	OVERLOAD(print, str)
-	// --- end setup --- //
-
-	INIT(i32, i, 0)
-	INIT(str, s, "string")
-
-	fmt(&i);
-	fmt(&s);
-
-	print(&i);
-	print(&s);
 
 	return 0;
 }
