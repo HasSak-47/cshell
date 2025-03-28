0a7e7609666b84cb45f63decdbc8e1a5ae934a86
arch machine <lilith.alanis.255@gmail.com>
2025-03-28 14:06:28 -0600

    better debug

---
+++ include/debug.h
@@ -0,0 +1,8 @@
+#ifndef __debug_h__
+#define __debug_h__
+
+void debug_printf(const char* fmt, ...);
+void set_debug();
+void unset_debug();
+
+#endif
--- src/debug.c
+++ src/debug.c
@@ -1,4 +1,22 @@
-#ifndef __debug_h__
-#define __debug_h__
+#include <stdarg.h>
+#include <stdio.h>
 
-#endif
+#include <debug.h>
+#include <state.h>
+
+static void __debug_printf_set_debug(const char* fmt, va_list args){
+    vprintf(fmt, args);
+}
+
+static void __debug_printf_unset_debug(const char* fmt, va_list args){}
+
+typedef void (*__debug_printf)(const char*, va_list args);
+
+static __debug_printf __functions[] = {__debug_printf_unset_debug, __debug_printf_set_debug};
+
+void debug_printf(const char* fmt, ...){
+    va_list(args);
+    va_start(args, fmt);
+    __functions[debug](fmt, args);
+    va_end(args);
+}
