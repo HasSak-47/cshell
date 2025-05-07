8e3de8064cab906e6e47dfea8fe1f54bb1019c79
arch machine <lilith.alanis.255@gmail.com>
2025-05-07 01:10:30 -0600

    set rust to nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,50 @@
-use std::{ffi::{c_char, c_int, CStr}, ptr::null};
+use std::{
+    ffi::{CStr, c_char, c_int},
+    ptr::null,
+};
 
 use clap::Parser;
 
 #[derive(Parser, Debug, Default)]
 #[command(version, about, long_about = None)]
-struct Args{
+struct Args {
     #[arg(short, long, default_value_t = false)]
     debug: bool,
     script: Option<String>,
 }
 
 #[unsafe(no_mangle)]
-unsafe extern "C" fn parse_args(argc: c_int, argv: *const *const c_char) -> *mut Args{
+unsafe extern "C" fn parse_args(argc: c_int, argv: *const *const c_char) -> *mut Args {
     let mut args = Vec::new();
-    unsafe{
-        for i in 0..(argc as usize){
+    unsafe {
+        for i in 0..(argc as usize) {
             let arg = CStr::from_ptr(*(argv.add(i)));
-            if let Ok(s) = arg.to_str(){
+            if let Ok(s) = arg.to_str() {
                 args.push(s.to_string());
             }
         }
     }
-
 
     let args = Args::parse_from(args);
     return Box::into_raw(Box::new(args));
 }
 
 #[unsafe(no_mangle)]
-unsafe extern "C" fn is_debug(args: *const Args) -> bool{
+unsafe extern "C" fn is_debug(args: *const Args) -> bool {
     if args.is_null() {
         return false;
     }
-    unsafe{
+    unsafe {
         return (&*args).debug;
     }
 }
 
 #[unsafe(no_mangle)]
-unsafe extern "C" fn get_script(args: *const Args) -> *const c_char{
+unsafe extern "C" fn get_script(args: *const Args) -> *const c_char {
     if args.is_null() {
         return null();
     }
-    unsafe{
+    unsafe {
         if let Some(s) = &(&*args).script {
             return s.as_ptr() as *const c_char;
         }
@@ -54,8 +56,8 @@ unsafe extern "C" fn get_script(args: *const Args) -> *const c_char{
 #[unsafe(no_mangle)]
 unsafe extern "C" fn free_args(args: *mut Args) {
     if !args.is_null() {
-        unsafe{
-            drop( Box::from_raw(args) );
+        unsafe {
+            drop(Box::from_raw(args));
         }
     }
 }
