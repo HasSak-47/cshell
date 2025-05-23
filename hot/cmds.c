5dadf9dfbb59bfc84b41145fbe0fe8baab7b3467
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 23:24:50 -0600

    idk

--- hot/cmds.c
+++ hot/cmds.c
@@ -1,3 +1,5 @@
+// here all the Luall.api functions are loaded
+//
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
@@ -52,11 +54,18 @@ static int api_execp(lua_State *L){
         const char* arg = lua_tostring(L, i);
         printf("arg: %s\n", arg);
     }
-
+    const char* path = getenv("path");
+    if(path == NULL){
+        last_return_code = -1;
+        return 0;
+    }
     return 0;
 }
 
-static int api_add_env(lua_State* L){
+static int api_set_env(lua_State* L){
+    const char* name = lua_tostring(L, 1);
+    const char* val = lua_tostring(L, 2);
+    setenv(name, val, 1);
     return 0;
 }
 
@@ -65,12 +74,15 @@ struct luaL_Reg api[] = {
     {"cd", api_cd},
     {"reload", api_reload},
     {"exec", api_execp},
+    {"add_env", api_set_env},
 
     {NULL, NULL}
 };
 
+// "constructor"
 struct luaL_Reg* get_api(){
     return api;
 }
 
+// "destructor"
 void take_api(struct luaL_Reg* api){ }
