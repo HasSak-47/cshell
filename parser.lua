fe9b451681daa15394dd0d978eb85a57862b2bb9
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 23:24:50 -0600

    idk

--- parser.lua
+++ parser.lua
@@ -1,9 +1,64 @@
 
+---@alias TokenType "statement"
+---| "process"
+---| "pipe"
+---| "redirection"
+---| "fd"
+---| "string"
+---@alias Statement {1:Process, 2:(Pipe | Redir(Fd | string) Process)[]}
+---@alias Process string[]
+---@alias Pipe "|"|"|&"
+---@alias Redir ">"|">>"|"<"|"<<"
+---@alias Fd "&0"|"&1"|"&2"
+
+-- <statement> ::= <process> ( (<pipe> | <redirection> (<fd> | <string>)) <process> )*
+-- <process>   ::= <string> (<string>)*
+-- <pipe>      ::= "|" | "|&"
+-- <redirection> ::= "<" | ">" | ">>" | "<<"
+-- <fd>        ::= "&0" | "&1" | "&2"
+-- <string>    ::= '"' [^"]* '"'
+-- | "'" [^']* "'"
+-- | [^"' \t\n|&<>]+
+
+
+local pipe_operators = {
+    "|",
+    "|&",
+}
+
+local redir_operators = {
+    "<",
+    "<<",
+    ">",
+    ">>",
+}
+
+local filedesc = {
+    "&0",
+    "&1",
+    "&2",
+}
+
+---@param tokens string[]
+---@return Process
+local function take_process(tokens)
+    return {}
+end
+
+---@param tokens string[]
+---@return Statement
+local function take_statement(tokens)
+    ---@type Process
+    local process = {}
+    ---@type Statement
+    local statement = {process}
+end
 
 ---@param input string
 local function tokenize(input)
     local i = 1
     local len = #input
+    ---@type string[]
     local tokens = {}
 
     while i <= len do
@@ -21,13 +76,13 @@ local function tokenize(input)
                 i = i + 1
             end
             -- remove quotes
-            table.insert(tokens, {val = input:sub(start + 1, i - 2), type = "string"})
+            table.insert(tokens, input:sub(start + 1, i - 2));
         elseif not char:match("%s") then
             local start = i
             while i <= len and not input:sub(i, i):match("%s") do
                 i = i + 1
             end
-            table.insert(tokens, {val = input:sub(start, i - 1), type = "undetermined"})
+            table.insert(tokens, input:sub(start, i - 1))
         else
             i = i + 1
         end
@@ -36,93 +91,24 @@ local function tokenize(input)
     return tokens
 end
 
----@param line string
-local function handle_shelllike(line)
-    local tokens = tokenize(line)
-    ---@type string[]
-    local args = {}
-    for _, token in pairs(tokens) do
-        table.insert(args, token.val)
-    end
+local function parser()
+    error('in testing...');
+end
 
-    local cmd = table.remove(args, 1)
-    if cmd == nil then
-        -- Luall.api.set_error(-1)
-        return
-    end
-
-    if Luall.util[cmd] ~= nil then
-        Luall.util[cmd](args)
-    elseif Luall.overwrite[cmd] ~= nil then
-        Luall.overwrite[cmd](args)
-    elseif Luall.api[cmd] ~= nil then
-        if #args == 0 then
-            Luall.api[cmd]()
-        else
-            Luall.api[cmd](table.unpack(args))
-        end
-    else
-
-        --- @param var string
-        local function parse_env(var)
-            local result = {}
-            for part in string.gmatch(var, "([^:]+)") do
-                table.insert(result, part)
-            end
-            return result
-        end
-
-        -- check all possible dirs in which cmd could be
-        local possible_locs = parse_env(Luall.vars.env.PATH)
-        local target_cmd = ''
-        for _, path in pairs(possible_locs) do
-            local candidate = path ..'/' .. cmd
-            if Luall.api.exists(candidate) then
-                target_cmd = candidate
-                break
-            end
-        end
-
-        local ok, _ = pcall(Luall.api['exec'], target_cmd, table.unpack(args))
-        if not ok then
-            print('cmd not found')
-        end
+--- testing function
+local function main()
+    local tokens = tokenize("echo 'test this out' this is wack >> out.txt");
+    for _, val in pairs(tokens) do
+        print(val)
+        
     end
 end
 
----@param line string
-local function handle_singleline(line)
-    local start  = line:find('lua')
-    if start == 1 then
-        line = line:gsub('lua', '')
-        Luall.util.lua({line})
-    else -- run "normal shell"
-        handle_shelllike(line)
-    end
-end
-
----@param input string
-local function parser (input)
-    if input == nil or input == '' then
-        return;
-    end
-
-    local lines = {}
-    local i = 1
-    -- split into lines
-    for line in input:gmatch('[^\n]*') do
-        if line == '' then
-            goto continue
-        end
-        lines[i] = line
-        i = i + 1
-        ::continue::
-    end
-
-    if #lines then -- single line
-        handle_singleline(lines[1])
-    else -- multi line
-    end
+if not package.loaded[...] then
+    main()
+else
+    -- File is being required as a module
+    -- No action needed
 end
 
 return parser
