c44e2fc9e5276efd9e8324f4c2bb3933fb553e17
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 23:24:50 -0600

    adding vector

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,8 @@
 #ifndef __UTILS__
 #define __UTILS__
 
+#include <stddef.h>
+
 #define unrecoverable_error(msg)\
     __error_msg(__LINE__, __FILE__, "[ERROR @ %s : %d]: %s\n", msg, -1)
 
@@ -18,5 +20,28 @@ void __error_msg(int line, char* file, char* fmt, char* msg, int code);
 
 void set_to_foreground();
 
+struct __Vector{
+    void* data;
+    size_t len;
+    size_t cap;
+};
+
+#define DefineVector(Name, T) \
+struct Vector##Name{ \
+    T* data;   \
+    size_t len;   \
+    size_t cap;   \
+};
+
+void __vector_push(struct __Vector* v, void* data, size_t size);
+void __vector_pop (struct __Vector* v, size_t size);
+
+#define vector_push(vector, data){ \
+    typeof(data) __vector_data; \
+    __vector_push((struct __Vector)&vector, &__vector_data, sizeof(*vector.data))\
+}
+
+#define vector_pop(vector)\
+    __vector_pop((struct __Vector)&vector, sizeof(*vector.data))
 
 #endif
