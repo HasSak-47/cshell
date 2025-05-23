ee727b7e9605c4512315850966de3a4a9d86a3a5
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 23:24:51 -0600

    waos

--- src/clangd.py
+++ src/clangd.py
@@ -49,24 +49,51 @@ def open_document(lsp:Popen, name: str):
 
     return lsp_read_message(lsp)
 
-def get_all_symbols(lsp:Popen):
-    lsp_write_message(lsp, {
-        'jsonrpc': '2.0',
-        'id': 1,
-        'method' : '$/progress',
-    })
+def get_all_symbols(lsp:Popen, f: None | str=None):
+    filters = {
+	    'File' : 1,
+	    'Module' : 2,
+	    'Namespace' : 3,
+	    'Package' : 4,
+	    'Class' : 5,
+	    'Method' : 6,
+	    'Property' : 7,
+	    'Field' : 8,
+	    'Constructor' : 9,
+	    'Enum' : 10,
+	    'Interface' : 11,
+	    'Function' : 12,
+	    'Variable' : 13,
+	    'Constant' : 14,
+	    'String' : 15,
+	    'Number' : 16,
+	    'Boolean' : 17,
+	    'Array' : 18,
+	    'Object' : 19,
+	    'Key' : 20,
+	    'Null' : 21,
+	    'EnumMember' : 22,
+	    'Struct' : 23,
+	    'Event' : 24,
+	    'Operator' : 25,
+	    'TypeParameter' : 26,
+    }
 
     lsp_write_message(lsp, {
         'jsonrpc': '2.0',
         'id': 1,
         'method' : 'workspace/symbol',
-        'params' :{ 'query': '' }
+        'params' :{ 'query': '', }
     })
 
-    return lsp_read_message(lsp)
+    syms = lsp_read_message(lsp)
+    if f != None and f in filters:
+        syms['result'] = list(filter(lambda x: x['kind'] == filters[f], syms['result']))
+    
+    return syms
 
 def init_clang():
-    args = ['clangd', '--compile-commands-dir=.', '--log=info']
+    args = ['clangd', '--compile-commands-dir=.', '--log=error']
     print(args)
     clangd = Popen(args, stdin=PIPE, stdout=PIPE)
     if clangd.stdin == None or clangd.stdout == None:
@@ -83,14 +110,13 @@ def init_clang():
         }
     })
 
-    # Ok message
+    # Read Ok message
     lsp_read_message(clangd)
 
     for file in listdir('hot'):
         open_document(clangd, f'hot/{file}')
 
     # from time import sleep
-    pprint(get_all_symbols(clangd))
-
+    pprint(get_all_symbols(clangd, 'Function'))
 
     return clangd
